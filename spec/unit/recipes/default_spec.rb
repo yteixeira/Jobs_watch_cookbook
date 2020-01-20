95be518271f37b8983dcdc4b91963d351902aa45
Ygor Teixeira <yteixeira@spartaglobal.com>
2020-01-20 16:39:03 +0000

    changes made to unit test

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -10,7 +10,13 @@ describe 'it-job-watch::default' do
   context 'When all attributes are default, on Ubuntu 18.04' do
     # for a complete list of available platforms and versions see:
     # https://github.com/chefspec/fauxhai/blob/master/PLATFORMS.md
-    platform 'ubuntu', '18.04'
+
+    let(:chef_run) do
+     # for a complete list of available platforms and versions see:
+     # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
+     runner = ChefSpec::SoloRunner.new(platform: 'ubuntu', version: '18.04')
+     runner.converge(described_recipe)
+    end
 
     it 'converges successfully' do
       expect { chef_run }.to_not raise_error
